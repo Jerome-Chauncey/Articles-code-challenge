8332f299490e2e5782353753dee38b6af79242d0
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-26 21:40:32 +0300

    [Mods]: setup_db.py > connection.py > [Creates]: Tables: > articles > authors > magazines

--- scripts/setup_db.py
+++ scripts/setup_db.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# 👇 This makes sure Python can find the lib/ folder
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from lib.db.connection import engine
